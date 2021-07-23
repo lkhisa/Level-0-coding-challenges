41c900baff345ac5c44c56789bcae7a320abc4e8
Loretta Khisa <61622885+lkhisa@users.noreply.github.com>
2021-07-24 01:17:39 +0300

    Update task6.js

--- task6.js
+++ task6.js
@@ -1,11 +1,5 @@
-function maximum(num1, num2, num3) {
-    if (num1 > num2 && num1 > num3) {
-        return num1;
-    } else if (num2 > num1 && num2 > num3) {
-        return num2;
-    } else {
-        return num3;
-    }
+function maximum() { 
+    return Math.max.apply(Math, arguments);   
 }
 console.log(maximum(9, 11, 2));
 console.log(maximum(1, 5, 2));
