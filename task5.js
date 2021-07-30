64eff885752bec19085367618518992e3ca538e3
Loretta Khisa <61622885+lkhisa@users.noreply.github.com>
2021-07-30 09:26:12 +0300

    Update task5.js

--- task5.js
+++ task5.js
@@ -1,6 +1,6 @@
 function areaOfTriangle(side1, side2, side3) {
     var s = (side1 + side2 + side3)/2; //s is the semiperimeter.
     var area = Math.sqrt(s *(s - side1) * (s - side2) * (s - side3)); 
-    return area + " square units";
+    return area.toFixed(2) + " square units";
 }
 console.log(areaOfTriangle(3, 4, 5));
